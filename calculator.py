cc12a08d6354777a2288365d122c13cf6a67b7ff
Kristin Bagnall <kristinbagnall@gmail.com>
2021-01-22 04:50:07 +0000

    build functions

--- calculator.py
+++ calculator.py
@@ -4,38 +4,53 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 while True:
-  user_input = input("Enter in your equation -->")
+  user_input = input("Enter in your equation --> ")
   tokens = user_input.split(' ')
 
+# defines variables
   user_command = tokens[0].lower()
   
   if user_command == 'q':
       print("You are done")
       break
   
-  num1 = float(tokens[1])
-  num2 = float(tokens[2])
+  elif len(tokens) == 2:
+      num1 = float(tokens[1])
+      num2 = 0
   
+  elif len(tokens) >= 3:
+      num1 = float(tokens[1])
+      num2 = float(tokens[2])
+  
+  else:
+      print('aahhhh')
+  
+
+# executes functions
+
   if user_command == 'pow':
       print(power(num1, num2))
 
   elif user_command == '+':
-      print(add(num1, num2))  
+      print(add(num1, num2))
 
-# > + 1 2
-# 3.0
-# > - 10 5
-# 5.0
-# > * 2 3
-# 6.0
-# > / 7 2
-# 3.5
-# > square 2
-# 4.0
-# > cube 3
-# 27.0
-# > pow 2 5
-# 32.0
-# > mod 10 3
-# 1.0
-# > q
+  elif user_command == '-':
+      print(subtract(num1, num2))
+
+  elif user_command == '*':
+      print(multiply(num1, num2))  
+
+  elif user_command == '/':
+      print(divide(num1, num2))
+
+  elif user_command == 'square':
+      print(square(num1))  
+
+  elif user_command == 'cube':
+      print(cube(num1))
+
+  elif user_command == 'mod' or user_command == '%':
+      print(mod(num1, num2))  
+
+  else:
+      print("Wrong input!")
